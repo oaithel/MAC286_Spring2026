29a4ab06120ed7a393161abcd5499686ea553d3b
ProfOmar <oaithel@yahoo.com>
2026-03-12 12:32:46 -0400

    3/12/2026

---
+++ src/com/mac286/stacks/PracticeStack.java
@@ -0,0 +1,33 @@
+package com.mac286.stacks;
+//TODO: HW3
+public class PracticeStack {
+    //design a static method that generates a stack of specific
+    // number of integers (num) between -max and +max.
+    public static OurStack<Integer> generateStack(int n, int max) {
+        //create a stak of integers
+        //create a random number generator
+
+        //loop n times
+        //generate a number between -max and +max
+        //push it to the stack
+
+        //return the stack
+    }
+
+
+    public static void main(String[] args) {
+        //generate a stack of 20 integers between -300 and +300.
+
+        //display it
+
+        //using two additional helper stacks (in total three stacks)
+        //reorganize the original stack so that numbers smaller than -100 go
+        //to the bottom of the original stack, numbers betwen -100 and +100
+        //in the middle and number larger than 100 to the top while respecting
+        //the relative order in which they were in the original stack.
+        //Example input [230, -20, 130, -135, 98, -200, -80, 156, 34, -230]
+        //output [-135, -200, -230, -20, 98, -80, 34, 230, 130, 156]
+
+    }
+
+}
