31ffdd357821350d7c1ea053a8477e6bc7e23123
ProfOmar <oaithel@yahoo.com>
2026-03-12 11:35:13 -0400

    3/12/2026

---
+++ src/com/mac286/stacks/wStack.java
@@ -0,0 +1,21 @@
+package com.mac286.stacks;
+/*
+Implement the stack as a wrapper around an OurVector Object
+ */
+public class wStack<T> {
+    //have a reference to OurVector object as private
+
+    //default constructor. Create an OurVector object
+
+    //size() forward the call to OurVector
+
+    //isEmpty() forward the call to OurVector
+
+    //push forward the call
+
+    //pop forward the call
+
+    //peek() forward the call
+
+    //toString forward the call
+}
