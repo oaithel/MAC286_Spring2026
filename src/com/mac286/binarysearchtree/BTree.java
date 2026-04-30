267db65d9b1baf6e2fce5eb9d68693ff50155ce2
Prof. Omar <oaithel@yahoo.com>
2026-04-30 11:59:57 -0400

    4/30/2026

--- src/com/mac286/binarysearchtree/BTree.java
+++ src/com/mac286/binarysearchtree/BTree.java
@@ -64,14 +64,98 @@ public class BTree <K extends Comparable, T>{
         }
 
         //start with a temporary reference to root
+        Node<K, T> temp = root;
         //forever
+        while(true) {
             //if k is less than the key at temp then
+            if(k.compareTo(temp.getKey()) < 0) {
                 //if there is something on the left, go left
+                if(temp.getLeft()!= null){
+                    temp = temp.getLeft();
+                    continue; //go back to while
+                }
                 //else add the node to the left, increase size, get out
-            //else (k is larger
+                temp.setLeft(n);
+                size++;
+                return; //breaks the loop
+            }else {
+                //else (k is larger
                 //if there is something on the right, go right
+                if(temp.getRight() != null){
+                    temp = temp.getRight();
+                    continue;
+                }
                 // else add the node to the right, increase size, get out
+                temp.setRight(n);
+                size++;
+                return;
+            }
+        }
 
+    }
+    //Tree traversal
+    public void preOrderTraversal(){
+        System.out.println("---------------PreOrder----------");
+        preOrder(root);
+        System.out.println();
+    }
+    private void preOrder(Node<K, T> n){
+        if(n == null){
+            return;
+        }
+        System.out.print("  " + n);
+        //explore the left subtree
+        preOrder(n.getLeft());
+        //explore the right subtree
+        preOrder(n.getRight());
+    }
+    public void inOrderTraversal(){
+        System.out.println("---------------inOrder----------");
+        inOrder(root);
+        System.out.println();
+    }
+    private void inOrder(Node<K, T> n){
+        if(n == null){
+            return;
+        }
+        //explore the left subtree
+        inOrder(n.getLeft());
+        //visit the root
+        System.out.print("  " + n);
+        //explore the right subtree
+        inOrder(n.getRight());
+    }
+    public void postOrderTraversal(){
+        System.out.println("---------------postOrder----------");
+        postOrder(root);
+        System.out.println();
+    }
+    private void postOrder(Node<K, T> n){
+        if(n == null){
+            return;
+        }
+        //explore the left subtree
+        postOrder(n.getLeft());
+        //explore the right subtree
+        postOrder(n.getRight());
+        //visit the root
+        System.out.print("  " + n);
+    }
+    //returns the reference to the node with key k
+    private Node<K, T> find(K k){
+        return null;
+    }
+    //returns refrence to smallest on Right
+    private Node<K, T> smallestOnRight(Node<K, T> n){
+        return null;
+    }
+    //returns refrence to largest on left
+    private Node<K, T> largestOnRight(Node<K, T> n){
+        return null;
+    }
+    private Node<K, T> parentOf(K k){
+
+        return null;
     }
 
 }
--- src/com/mac286/binarysearchtree/BTreeTester.java
+++ src/com/mac286/binarysearchtree/BTreeTester.java
@@ -2,11 +2,25 @@ package com.mac286.binarysearchtree;
 
 public class BTreeTester {
     public static void main(String[] args) {
-        String st = "Hz there";
-        String st1 = "How";
-        Integer i= 20;
-        Integer s = 25;
-        System.out.println("i and s cimpared: " + i.compareTo(s));
-        System.out.println("comparing the strings: " + st.compareTo(st1));
+       BTree<Integer, String> T = new BTree<>();
+       T.insert(25, "Hi25");
+       T.insert(13, "Hi13");
+       T.insert(28, "H28");
+       T.insert(17, "Hi17");
+       T.insert(21, "Hi21");
+       T.insert(15, "Hi15");
+       T.insert(12, "Hi12");
+       T.insert(30, "Hi30");
+       T.insert(26, "Hi26");
+       T.insert(35, "Hi35");
+       T.insert(31, "Hi31");
+       T.preOrderTraversal();
+        T.inOrderTraversal();
+        T.postOrderTraversal();
+
+
+
+
+
     }
 }
