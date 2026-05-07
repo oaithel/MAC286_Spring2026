f593dea7f6b0690aca01bc50e901654bc145067f
ProfOmar <oaithel@yahoo.com>
2026-05-07 12:22:11 -0400

    5/7/2026

---
+++ src/com/mac286/heaps/PracticeHeaps.java
@@ -0,0 +1,8 @@
+package com.mac286.heaps;
+/*
+TODO: HW8
+Generate an array of 10 random integers. Using a Heap, order the array
+in an descending order (largest first).
+ */
+public class PracticeHeaps {
+}
