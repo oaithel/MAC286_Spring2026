83f6af45a5fa328b93d1edc832439a2a24f35ce5
ProfOmar <oaithel@yahoo.com>
2026-05-19 11:24:01 -0400

    5/19/2025

--- src/com/mac286/hashtables/HashtableExample.java
+++ src/com/mac286/hashtables/HashtableExample.java
@@ -9,25 +9,25 @@ import java.util.Set;
 
 public class HashtableExample {
     public static void main(String[] args) {
-        Hashtable<Integer, String> Tab = new Hashtable<>(20, 0.8f);
+        OurHashtable<Integer, String> Tab = new OurHashtable<>(20, 0.8f);
         Tab.put(11209, "BayRidge");
         Tab.put(11101, "Long Island City");
         Tab.put(10000, "Manhattan");
         System.out.println("Tab: " + Tab.toString());
-        System.out.println("ramoving zip 10000: " + Tab.remove(10000));
-        System.out.println("Tab: " + Tab.toString());
+        //System.out.println("ramoving zip 10000: " + Tab.remove(10000));
+        //System.out.println("Tab: " + Tab.toString());
         Tab.put(11111, "No Clue");
         System.out.println("Value at 11111: "+ Tab.get(11111));
         Tab.put(11377, "Woodside");
         System.out.println("Tab: " + Tab.toString());
         //Going through elements of a hashtable.
         //1- get the set of the keys
-        Set<Integer> keys = Tab.keySet();
+        /*Set<Integer> keys = Tab.keySet();
         //use a for loop to go throught the set of keys
         for(Integer k: keys){
             System.out.println(k + " = " + Tab.get(k));
         }
-
+*/
         System.out.println("Tab: " + Tab);
         Tab.put(11111, "someting else");
         System.out.println("Tab: " + Tab);
--- src/com/mac286/hashtables/OurHashtable.java
+++ src/com/mac286/hashtables/OurHashtable.java
@@ -4,7 +4,14 @@ public class OurHashtable <K, T>{
     private class Node<KK, TT>{
         private KK key;
         private TT data;
-
+        public Node(){
+            key = null;
+            data = null;
+        }
+        public Node(KK k, TT d){
+            key = k;
+            data = d;
+        }
         public KK getKey() {
             return key;
         }
@@ -50,24 +57,68 @@ public class OurHashtable <K, T>{
     private int hashFunction(K k){
         //take the absolute value of java hashcode of k
         // and use modulo of the array length
+        int index  = Math.abs(k.hashCode())%H.length;
+        return index;
     }
     public void put(K k, T d){
         //find the index using the hash function
+        int ind = hashFunction(k);
         //as long as H[index] is not null
+        while(H[ind] != null) {
             //compare key to key of this elements if equal, replace
             //data and return
+            if(k.equals(H[ind].key)){
+                H[ind].setData(d);
+                return;
+            }
             //if not go to next element
-
+            ind = ind + 1;
+        }
         //If we get here at the end of the loop, means it does not exist
-            //create a node.
-            //as long as H[index modulo length] is not null, mode to next
-                //store the node in the current element.
+        //create a node.
+        Node<K, T> n = new Node<>(k, d);
+        //store the node in the current element.
+        H[ind] = n;
+        size++;
+    }
+    public T get(K k){
+        int ind = hashFunction(k);
+        //as long as H[index] is not null
+        while(H[ind] != null) {
+            //compare key to key of this elements if equal, replace
+            //data and return
+            if(k.equals(H[ind].key)){
+                return H[ind].getData();
+            }
+            //if not go to next element
+            ind = ind + 1;
+        }
+        return null;
     }
     public String toString(){
+        if(this.isEmpty()){
+            return "{}";
+        }
         //start at 0, while empty move until we find the first
         //non empty element.
-            //Create a string with "{" and first element
-        //continue the loop from that point on until the end
+        int i = 0;
+        while(H[i] == null)
+            i++;
+        String st = "{" + H[i];
+        int s = 1;
+        i++;
+        for(int j = i+1; j < H.length && s < size; j++) {
+            //continue the loop from that point on until the end
             //if current element is not null, add "," + element to it.
+            if(H[j] != null){
+                s++;
+                st += ", " + H[j];
+            }
+        }
+        return st + "}";
+    }
+    public T remove(){
+
+        return null;
     }
 }
